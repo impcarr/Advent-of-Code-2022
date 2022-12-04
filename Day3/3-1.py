9114f274a37809ade437705d2d857b604c81fdc2
“Ian” <ianmpcarr@gmail.com>
2022-12-03 19:22:11 -0700

    Solved day 3

---
+++ Day3/3-1.py
@@ -0,0 +1,13 @@
+from itertools import count
+sacks = open("input.txt", "r").read().splitlines()
+total = 0
+for sack in sacks:
+    s1, s2 = set(sack[:len(sack)//2]), set(sack[len(sack)//2:])
+    res = set.intersection(s1,s2).pop()
+    if res.isupper():
+        total += ord(res)-ord("A")+27
+    else:
+        total += ord(res)-ord("a")+1
+    
+print(total)
+
---
+++ Day3/3-2.py
@@ -0,0 +1,18 @@
+from itertools import zip_longest
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
+sacks = open("input.txt", "r").read().splitlines()
+total = 0
+for s1, s2, s3 in grouper(3,sacks):
+    s1, s2, s3 = set(s1), set(s2), set(s3)    
+    res = set.intersection(s1,s2,s3).pop()
+    if res.isupper():
+        total += ord(res)-ord("A")+27
+    else:
+        total += ord(res)-ord("a")+1
+    
+print(total)
+
